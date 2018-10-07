1441cb98aa4c8519c5bfaaedd96f3734b1a0795a
michel lima <mmichel.lima@hotmail.com.br>
2018-10-07 18:34:35 +0100

    App01 concluida

---
+++ CEP/ConsultarCep/ConsultarCep/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using ConsultarCep.Service;
+using ConsultarCep.Service.Model;
+using System;
+using Xamarin.Forms;
+
+namespace ConsultarCep
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BtnBuscarCep.Clicked += BuscarCep;
+        }
+
+        private void BuscarCep(object sender, EventArgs args)
+        {
+            var cep = Cep.Text.Trim();
+            if (isValidCEP(cep))
+            {
+                try
+                {
+                    Endereco endereco = ViaCepService.BuscarEnderecoViaCEP(cep);
+
+                    if (endereco != null)
+                    {
+                        lbResultado.Text = string.Format("Endereço: {0}, {1}, {2}", endereco.Localidade, endereco.Uf, endereco.Logradouro);
+                    }
+                    else
+                    {
+                        DisplayAlert("ERRO", "Endereço não encontrado para o cepe informado: " + cep, "ok");
+                    }
+                }
+                catch (Exception e)
+                {
+                    DisplayAlert("ERRO", "Erro Crítico", "ok");
+                }
+            }
+        }
+
+        private bool isValidCEP(string cep)
+        {
+            var NovoCep = 0;
+            if (!int.TryParse(cep, out NovoCep))
+            {
+                DisplayAlert("ERRO", "CEP inválido, deve conter apenas numeros", "ok");
+                return false;
+            }
+
+            if (cep.Length != 8)
+            {
+                DisplayAlert("ERRO", "CEP inválido, deve conter 8 caracteres", "ok");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ CEP/ConsultarCep/ConsultarCep/Service/ViaCepService.cs
@@ -0,0 +1,26 @@
+﻿using ConsultarCep.Service.Model;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace ConsultarCep.Service
+{
+    public class ViaCepService
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient cliente = new WebClient();
+            string Conteudo = cliente.DownloadString(NovoEnderecoURL);
+
+            Endereco endereco = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            if (endereco.Cep == null)
+                return null;
+
+            return endereco;
+        }
+    }
+}
